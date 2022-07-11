0433bf0d887dae6ca7c35a491a9ce00d4e0a2b6a
Marcos <MarcAnt@users.noreply.github.com>
2022-07-10 20:44:59 -0400

    Fix files an config

---
+++ index.js
@@ -0,0 +1,18 @@
+// Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+const employees = require("./api/employees");
+
+// Create GET request
+app.use("/api/employees", employees);
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
+module.exports = app;
